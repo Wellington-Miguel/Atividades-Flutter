9517c1cbb032b89c65f4dd2a8f16b2455fa9f965
Wellington-Miguel <wellington2010_silva@hotmail.com>
2022-02-03 21:46:02 -0300

    implemented capturetion data

--- teste/lib/main.dart
+++ teste/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MinhasTarefas());
 
@@ -7,15 +8,16 @@ class MinhasTarefas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: AddTarefa(),
     );
   }
 }
 
 class AddTarefa extends StatelessWidget {
-  const AddTarefa({Key? key}) : super(key: key);
-
+  AddTarefa({Key? key}) : super(key: key);
+  final TextEditingController _novatarefa = TextEditingController();
+  final TextEditingController _novadata = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,22 +30,90 @@ class AddTarefa extends StatelessWidget {
         ),
         child: Column(
           children: [
-            ImputTarefa(
-              'Nova Tarefa',
-              'Digite sua nova tarefa...',
-              Icon(Icons.app_registration_rounded),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: const BorderSide(
+                    width: 3,
+                    color: Colors.green,
+                  ),
+                ),
+                elevation: 10,
+                shadowColor: Colors.amber,
+                child: TextField(
+                  controller: _novatarefa,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    decorationColor: Colors.green,
+                  ),
+                  decoration: const InputDecoration(
+                      iconColor: Colors.amberAccent,
+                      icon: Icon(Icons.app_registration_rounded),
+                      hintText: 'Digite sua nova tarefa',
+                      labelText: 'Nova tarefa'),
+                ),
+              ),
             ),
-            ImputTarefa(
-              'Data de entrega',
-              'dia/mês/ano',
-              Icon(Icons.date_range),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: const BorderSide(
+                    width: 3,
+                    color: Colors.green,
+                  ),
+                ),
+                elevation: 10,
+                shadowColor: Colors.amber,
+                child: TextField(
+                  controller: _novadata,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    decorationColor: Colors.green,
+                  ),
+                  decoration: const InputDecoration(
+                      iconColor: Colors.amberAccent,
+                      icon: Icon(Icons.app_registration_rounded),
+                      hintText: 'Dara de entrega',
+                      labelText: 'Dia/mês/ano'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton.icon(
+                icon: Icon(
+                  Icons.verified,
+                  color: Colors.amber[50],
+                ),
+                style: ElevatedButton.styleFrom(
+                  elevation: 20,
+                  primary: Colors.teal[900],
+                  onPrimary: Colors.green,
+                  fixedSize: Size(200, 50),
+                ),
+                onPressed: () {
+                  final String tarefa = _novatarefa.text;
+                  final String data = _novadata.text;
+                },
+                label: Text(
+                  'Confirmar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.amber[50],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
       appBar: AppBar(
         title: const Text(
-          'Adicionar nova tarefa',
+          'Adicionar tarefa',
           style: TextStyle(fontSize: 30),
         ),
         backgroundColor: Colors.lightGreen[800],
@@ -53,41 +123,6 @@ class AddTarefa extends StatelessWidget {
           Icon(Icons.search),
           Padding(padding: EdgeInsets.symmetric(horizontal: 30)),
         ],
-      ),
-    );
-  }
-}
-
-class ImputTarefa extends StatelessWidget {
-  ImputTarefa(this.tite, this.subtite, this.icon, {Key? key}) : super(key: key);
-  String tite;
-  String subtite;
-  Icon icon;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-          side: const BorderSide(
-            width: 3,
-            color: Colors.green,
-          ),
-        ),
-        elevation: 10,
-        shadowColor: Colors.amber,
-        child: TextField(
-          style: const TextStyle(
-            fontSize: 20,
-            decorationColor: Colors.green,
-          ),
-          decoration: InputDecoration(
-              iconColor: Colors.amberAccent,
-              icon: icon,
-              hintText: subtite,
-              labelText: tite),
-        ),
       ),
     );
   }
@@ -162,6 +197,7 @@ class ListaTarefa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shadowColor: Colors.amber,
       child: Container(
         decoration: BoxDecoration(
           border: Border.all(color: Colors.green, width: 2),
