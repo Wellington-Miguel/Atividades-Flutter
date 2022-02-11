ab3bc36d6bfd08a26eb17826b8c253ad6d53014b
Wellington-Miguel <wellington2010_silva@hotmail.com>
2022-02-11 15:08:17 -0300

    add progressbar

---
+++ exercicio/lib/exerciseCubosAcademy/progressBar.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class progressBar extends StatefulWidget {
+  const progressBar({Key? key}) : super(key: key);
+
+  @override
+  _progressBarState createState() => _progressBarState();
+}
+
+class _progressBarState extends State<progressBar> {
+  Random random = Random();
+  int porcent = 0;
+  String info = 'Clique para fazer o upload';
+  MaterialColor progresscolor = Colors.blue;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Barra de progresso'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: progresscolor,
+              ),
+              onPressed: () {
+                setState(() {
+                  if (porcent <= 100) {
+                    porcent += random.nextInt(20);
+                  }
+                  if (porcent >= 100) {
+                    porcent = 100;
+                    progresscolor = Colors.green;
+                  }
+                  info = '${porcent.toString()}%';
+                });
+              },
+              child: const Text('Upload'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Text(info),
+          ),
+          SizedBox(
+            width: 200,
+            height: 5,
+            child: LinearProgressIndicator(
+              value: porcent / 100,
+              color: progresscolor,
+              backgroundColor: Colors.black,
+              semanticsLabel: 'Progresso',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- exercicio/lib/main.dart
+++ exercicio/lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:exercicio/exerciseCubosAcademy/menuDrawer.dart';
+import 'package:exercicio/exerciseCubosAcademy/progressBar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      const MaterialApp(debugShowCheckedModeBanner: false, home: menuDrawer()));
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: progressBar(),
+    ),
+  );
 }
