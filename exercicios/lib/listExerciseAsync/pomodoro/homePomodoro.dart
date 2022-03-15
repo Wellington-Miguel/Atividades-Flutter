aea392b43b9e83047e6b520eb85667a298d8beb8
Wellington-Miguel <wellington2010_silva@hotmail.com>
2022-03-14 23:27:17 -0300

    update execise pomodoro

--- exercicios/lib/listExerciseAsync/pomodoro/homePomodoro.dart
+++ exercicios/lib/listExerciseAsync/pomodoro/homePomodoro.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 enum StatePomodoro {
   start,
   off,
+  rest,
 }
 
 class HomePomodoro extends StatefulWidget {
@@ -21,7 +22,7 @@ class _HomePomodoroState extends State<HomePomodoro> {
   String textButtonTime = 'Iniciar pomodoro';
   double porcentProgress = 0;
   int time = 30;
-
+  bool count = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,61 +35,85 @@ class _HomePomodoroState extends State<HomePomodoro> {
         title: const Text('My Pomodoro'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(40),
-              child: SizedBox(
-                height: 40,
-                width: 200,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red[800],
-                    ),
-                    onPressed: () {
-                      addTask();
-                    },
-                    child: Text(textButtonTask)),
-              ),
-            ),
-            Stack(alignment: Alignment.center, children: [
-              SizedBox(
-                height: 300,
-                width: 300,
-                child: CircularProgressIndicator(
-                  strokeWidth: 40,
-                  value: porcentProgress,
-                  backgroundColor: Colors.black,
-                  color: Colors.red[900],
+        child: Padding(
+          padding: const EdgeInsets.all(70),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(40),
+                child: SizedBox(
+                  height: 40,
+                  width: 200,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: state == StatePomodoro.start ||
+                                state == StatePomodoro.off
+                            ? Colors.red[900]
+                            : Colors.green,
+                      ),
+                      onPressed: () {
+                        addTask();
+                      },
+                      child: Text(textButtonTask)),
                 ),
               ),
-              Text(state == StatePomodoro.off ? pomodoroTime : '00:$time',
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.red[900],
-                      fontWeight: FontWeight.bold)),
-            ]),
-            Padding(
-              padding: const EdgeInsets.all(40),
-              child: SizedBox(
-                height: 40,
-                width: 200,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red[800],
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        timePomodoro();
-                        state = StatePomodoro.start;
-                      });
-                    },
-                    child: Text(
-                      textButtonTime,
-                    )),
+              Stack(alignment: Alignment.center, children: [
+                SizedBox(
+                  height: 300,
+                  width: 300,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 40,
+                    value: porcentProgress,
+                    backgroundColor: Colors.black,
+                    color: state == StatePomodoro.start ||
+                            state == StatePomodoro.off
+                        ? Colors.red[900]
+                        : Colors.green,
+                  ),
+                ),
+                Text(state == StatePomodoro.off ? pomodoroTime : '00:$time',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: state == StatePomodoro.start ||
+                                state == StatePomodoro.off
+                            ? Colors.red[900]
+                            : Colors.green,
+                        fontWeight: FontWeight.bold)),
+              ]),
+              Padding(
+                padding: const EdgeInsets.all(40),
+                child: SizedBox(
+                  height: 40,
+                  width: 200,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: state == StatePomodoro.rest
+                            ? Colors.green
+                            : Colors.red[800],
+                      ),
+                      onPressed: count == true
+                          ? null
+                          : () {
+                              timePomodoro();
+                              if (time == 30) {
+                                setState(() {
+                                  state = StatePomodoro.start;
+                                  textButtonTime = 'Go Go Go';
+                                });
+                              } else if (time == 15) {
+                                setState(() {
+                                  state = StatePomodoro.rest;
+                                  textButtonTime = 'Hora de descansar';
+                                });
+                              }
+                            },
+                      child: Text(
+                        textButtonTime,
+                      )),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -98,9 +123,33 @@ class _HomePomodoroState extends State<HomePomodoro> {
     for (int i = time; i >= 0; i--) {
       await Future.delayed(const Duration(seconds: 1));
       time = i;
-      debugPrint(time.toString());
+      count = true;
+
+      if (state == StatePomodoro.rest) {
+        porcentProgress -= (3.3 / 100) * 2;
+      } else {
+        porcentProgress += 3.3 / 100;
+      }
+      debugPrint(porcentProgress.toString());
       setState(() {
         time;
+        porcentProgress;
+      });
+    }
+    if (time == 0 && state == StatePomodoro.start) {
+      setState(() {
+        time = 15;
+        state = StatePomodoro.rest;
+        textButtonTime = 'Iniciar descanso';
+        count = false;
+      });
+    } else if (time == 0 && state == StatePomodoro.rest) {
+      setState(() {
+        time == 30;
+        textButtonTask = 'Adicione uma tarefa';
+        textButtonTime = 'Iniciar pomodoro';
+        state = StatePomodoro.off;
+        count = false;
       });
     }
   }
