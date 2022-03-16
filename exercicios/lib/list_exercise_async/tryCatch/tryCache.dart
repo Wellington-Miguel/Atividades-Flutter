894526b271977d47a8c81f06d637bc187895798f
Wellington-Miguel <wellington2010_silva@hotmail.com>
2022-03-16 20:30:43 -0300

    update exercise try cache

--- exercicios/lib/list_exercise_async/tryCatch/tryCache.dart
+++ exercicios/lib/list_exercise_async/tryCatch/tryCache.dart
@@ -10,6 +10,12 @@ class TryCache extends StatefulWidget {
 class _TryCacheState extends State<TryCache> {
   final firstNumber = TextEditingController();
   final lastNumber = TextEditingController();
+  int valueOne = 0;
+  int valueTwo = 0;
+  int result = 0;
+  FocusNode focusOne = FocusNode();
+  FocusNode focusTwo = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +34,7 @@ class _TryCacheState extends State<TryCache> {
           Padding(
             padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
             child: TextFormField(
+              focusNode: focusOne,
               textAlign: TextAlign.center,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
@@ -38,6 +45,7 @@ class _TryCacheState extends State<TryCache> {
           Padding(
             padding: const EdgeInsets.all(40),
             child: TextFormField(
+              focusNode: focusTwo,
               textAlign: TextAlign.center,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
@@ -45,12 +53,49 @@ class _TryCacheState extends State<TryCache> {
               controller: lastNumber,
             ),
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Calcular'),
+          MaterialButton(
+            color: Theme.of(context).primaryColor,
+            onPressed: calcular,
+            child: const Text(
+              'Calcular',
+              style: TextStyle(color: Colors.white),
+            ),
           )
         ],
       ),
     );
+  }
+
+  void calcular() {
+    try {
+      valueOne = int.parse(firstNumber.text);
+      debugPrint(valueOne.toString());
+    } catch (e) {
+      menssagem("Digite um número");
+      FocusScope.of(context).requestFocus(focusOne);
+      return;
+    }
+    try {
+      valueTwo = int.parse(lastNumber.text);
+      debugPrint(valueTwo.toString());
+    } catch (e) {
+      menssagem("Digite um número");
+      FocusScope.of(context).requestFocus(focusTwo);
+      return;
+    }
+    result = valueOne + valueTwo;
+    menssagem('A soma dos números é $result');
+    firstNumber.text = '';
+    lastNumber.text = '';
+  }
+
+  void menssagem(String erro) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(erro),
+          );
+        });
   }
 }
